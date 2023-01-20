913681fcbcabc5207ff202404f31e8019ffb8ae4
Anastasia Simolova <simolovaai@gmail.com>
2023-01-20 20:07:04 +0300

    поправила тест

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.Configuration;
 import io.qameta.allure.junit4.DisplayName;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import pages.LoginPage;
@@ -10,10 +11,10 @@ import pages.RegPage;
 import user.User;
 import user.UserClient;
 
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverConditions.url;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.page;
+import static com.codeborne.selenide.WebDriverRunner.url;
 import static org.junit.Assert.assertTrue;
-import static pages.LoginPage.LOGIN_URL;
 
 public class RegistrationTest {
 
@@ -44,8 +45,9 @@ public class RegistrationTest {
         registrationPage.emailEnter(user.getEmail());
         registrationPage.passwordEnter(user.getPassword());
         registrationPage.register();
-        webdriver().shouldHave(url(LOGIN_URL));
-        auth = userClient.loginUser(user).extract().body().path("accessToken");
+        Assert.assertEquals("Url отличается от ожидаемого", LoginPage.LOGIN_URL, url());
+        Assert.assertEquals("Статус код отличается от ожидаемого", userClient.loginUser(user).extract().statusCode(), 200);
+        // auth = userClient.loginUser(user).extract().body().path("accessToken");
     }
 
     @Test
