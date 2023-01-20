c2cc7d9bab7f1bb1291ef9bd114100320e30bd01
Anastasia Simolova <simolovaai@gmail.com>
2023-01-20 20:00:14 +0300

    добавила в тесты проверку

--- src/test/java/ConstructorTest.java
+++ src/test/java/ConstructorTest.java
@@ -1,11 +1,11 @@
 import com.codeborne.selenide.Configuration;
 import io.qameta.allure.junit4.DisplayName;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import pages.MainPage;
 
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.Assert.assertTrue;
 import static pages.MainPage.MAIN_URL;
 
 public class ConstructorTest {
@@ -23,21 +23,24 @@ public class ConstructorTest {
     @DisplayName("Переход в раздел Булки")
     public void checkOfBunsButton() {
         MainPage main = open(MAIN_URL, MainPage.class);
-        assertTrue("Block is invisible", main.clickButtonBunsAndCheck());
+mainPage.clickButtonBunsAndCheck();
+        Assert.assertTrue(mainPage.selectInConstructorCheck("Булки"));
     }
 
     @Test
     @DisplayName("Переход в раздел Соусы")
     public void checkOfSaucesButton() {
         MainPage main = open(MAIN_URL, MainPage.class);
-        assertTrue("Block is invisible", main.clickButtonSaucesAndCheck());
+        mainPage.clickButtonSaucesAndCheck();
+        Assert.assertTrue(mainPage.selectInConstructorCheck("Соусы"));
     }
 
     @Test
     @DisplayName("Переход в раздел Начинки")
     public void checkOfFillingButton() {
         MainPage main = open(MAIN_URL, MainPage.class);
-        assertTrue("Block is invisible", main.clickButtonFillingAndCheck());
+        mainPage.clickButtonFillingAndCheck();
+        Assert.assertTrue(mainPage.selectInConstructorCheck("Начинки"));
     }
 
 }
